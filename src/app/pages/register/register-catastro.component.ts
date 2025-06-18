9abc6df37570173b8ad1f095ac4a1c0302323d77
Erika Achote <121589258+erli1a@users.noreply.github.com>
2025-06-17 22:36:24 -0500

    first commit

---
+++ src/app/pages/register/register-catastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-catastro',
+  imports: [],
+  templateUrl: './register-catastro.component.html',
+  styleUrl: './register-catastro.component.scss'
+})
+export class RegisterCatastroComponent {
+
+}
